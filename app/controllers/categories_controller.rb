c4fcd1ea5ae7202b20b60dc363f572d88062a95e
Rhonda Tinkham <rtinkham@live.com>
2017-05-05 03:36:42 +0000

    Added edit category feature

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -13,10 +13,24 @@ class CategoriesController < ApplicationController
   def create
     @category = Category.new(category_params)
     if @category.save
-      flash[:success] = "Category was created successfully"
+      flash[:success] = "Category was created successfully!"
       redirect_to categories_path
     else
       render 'new'
+    end
+  end
+  
+  def edit
+    @category = Category.find(params[:id])
+  end
+  
+  def update 
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:success] = 'Category name was successfully updated!'
+      redirect_to category_path(@category)
+    else
+      render 'edit'
     end
   end
  
@@ -32,7 +46,7 @@ class CategoriesController < ApplicationController
   
   def require_admin
     if !logged_in? || (logged_in? and !current_user.admin?)
-      flash[:danger] = "Only admins can perform that action"
+      flash[:danger] = "Only admins can perform that action!"
       redirect_to categories_path
     end
   end
